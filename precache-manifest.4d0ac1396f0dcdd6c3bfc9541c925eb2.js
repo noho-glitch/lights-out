c94723d858e87e432d17226efc859eb25f982aa7
Dave Nolan <dpn.nolan@gmail.com>
2020-03-19 14:58:29 -0400

    Updates

---
+++ precache-manifest.4d0ac1396f0dcdd6c3bfc9541c925eb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf5750cc2e72a0c85f6",
+    "url": "/lights-out/static/css/main.60a4c072.chunk.css"
+  },
+  {
+    "revision": "bbf5750cc2e72a0c85f6",
+    "url": "/lights-out/static/js/main.bbf5750c.chunk.js"
+  },
+  {
+    "revision": "306596ad124034e94b54",
+    "url": "/lights-out/static/js/1.306596ad.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "9348c7af293da015d698312d4afa4bab",
+    "url": "/lights-out/index.html"
+  }
+];
